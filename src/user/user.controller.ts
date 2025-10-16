c2f8de46f7ef6bc26b4c568425863bd7d9e8d388
Frank Feitosa <feitosfrank22@gmail.com>
2025-10-16 17:12:50 -0300

    feat: Prisma, Crud Simples User, Docker-Compose

---
+++ src/user/user.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { UsersService } from './user.service';
+
+@Controller('users')
+export class UsersController {
+   constructor(private readonly usersService: UsersService) { }
+
+   @Post()
+   create(@Body() data: any) {
+      return this.usersService.create(data);
+   }
+
+   @Get()
+   findAll(){
+      return this.usersService.findAll()
+   }
+
+   @Get(':id')
+   findOne(@Param('id') id: string){
+      return this.usersService.findOne(id)
+   }
+
+   @Put('id')
+   update(@Param('id') id: string, @Body() data:any){
+      return this.usersService.update(id, data)
+   }
+
+   @Delete(':id')
+   remove(@Param('id') id:string){
+      return this.usersService.remove(id)
+   }
+}
